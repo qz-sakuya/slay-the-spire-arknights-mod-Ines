e5c7cb258ff0851025eb6527cea5b63e2a8e7170
rv4xPen <11521764+rv4xpen@user.noreply.gitee.com>
2025-11-19 17:42:10 +0800

    2卡，未测试

---
+++ src/main/java/InesMod/cards/skill/EternalMigration.java
@@ -0,0 +1,56 @@
+package InesMod.cards.skill;
+
+import InesMod.cards.AbstractInesCard;
+import InesMod.cards.status.ShadowWhistle;
+import InesMod.characters.Ines;
+import InesMod.helpers.PathHelper;
+import InesMod.powers.DeadlyOpportunityPower;
+import InesMod.powers.EternalMigrationPower;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.watcher.SkipEnemiesTurnAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.WhirlwindEffect;
+
+
+
+/**
+ * 中文卡名：无尽迁徙
+ */
+public class EternalMigration extends AbstractInesCard {
+    public static final String ID = PathHelper.nameToId(EternalMigration.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
+
+    public EternalMigration() {
+        super(ID,
+                false,
+                cardStrings,
+                3,
+                CardType.SKILL,
+                CardRarity.RARE,
+                CardTarget.SELF,
+                Ines.Enums.INES_CARD);
+
+        this.cardsToPreview = new ShadowWhistle();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new EternalMigrationPower(p, -1), -1));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            upgradeBaseCost(2);
+        }
+    }
+}
---
+++ src/main/java/InesMod/cards/skill/SecretHRLetter.java
@@ -0,0 +1,51 @@
+package InesMod.cards.skill;
+
+import InesMod.cards.AbstractInesCard;
+import InesMod.cards.status.ShadowWhistle;
+import InesMod.characters.Ines;
+import InesMod.helpers.PathHelper;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+/**
+ * 中文卡名：人事部密信
+ */
+public class SecretHRLetter extends AbstractInesCard {
+    public static final String ID = PathHelper.nameToId(SecretHRLetter.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
+
+    public SecretHRLetter() {
+        super(ID,
+                true,
+                cardStrings,
+                3,
+                CardType.SKILL,
+                CardRarity.RARE,
+                CardTarget.SELF,
+                Ines.Enums.INES_CARD);
+
+        this.cardsToPreview = new ShadowWhistle();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // 遍历手牌
+        for (AbstractCard c : AbstractDungeon.player.hand.group) {
+            c.setCostForTurn(c.cost - 2); // 耗能-2
+        }
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            upgradeBaseCost(2);
+        }
+    }
+}
