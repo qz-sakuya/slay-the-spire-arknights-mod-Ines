9df9b841e0cecc269d9a28d998b20be62fcdf9d3
rv4xPen <11521764+rv4xpen@user.noreply.gitee.com>
2026-01-20 04:31:05 +0800

    结局图

--- src/main/java/InesMod/characters/Ines.java
+++ src/main/java/InesMod/characters/Ines.java
@@ -233,7 +233,8 @@ public class Ines extends CustomPlayer
     public ArrayList<CutscenePanel> getCutscenePanels() {
         ArrayList<CutscenePanel> panels = new ArrayList<>();
         // 有两个参数的，第二个参数表示出现图片时播放的音效
-        panels.add(new CutscenePanel("InesModResources/img/char/Victory1.png", "ATTACK_MAGIC_FAST_1"));
+        panels.add(new CutscenePanel("InesModResources/img/char/Victory1.png", "ATTACK_WHIRLWIND"));
+        panels.add(new CutscenePanel("InesModResources/img/char/Victory2.png"));
         return panels;
     }
 
